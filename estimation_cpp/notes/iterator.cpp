281cd798ad300b6e2c18742c708cc1d10288c872
kuubi3d <47939652+kuubi3d@users.noreply.github.com>
2021-05-01 18:06:12 -1000

    update

--- estimation_cpp/notes/iterator.cpp
+++ estimation_cpp/notes/iterator.cpp
@@ -3,37 +3,27 @@
 
 using namespace std;
 
-int main ()
-{
-    vector<int> estimator;
-
-    for (int i =1; i <=10; i++)
-    {
-        estimator.push_back(i);
-    }
-
-    for (int j = 0; j<10; j++)
-    {
-        cout << estimator[j] << endl;
-    }
-}   
+// :::: Variable definition
+int dt;
 
 int main ()
 {
     vector<int> predictedState;
     vector<int> curState;
+    
     for (int i =1; i <=10; i++)
     {
         predictedState.push_back(i);
-        curState.push_back(i+10)
-        
+        curState.push_back(i+10);
+        dt = i;
+        cout << i << endl;
     }
 
     for (int j = 0; j<10; j++)
     {
         cout << predictedState[j] << endl;
-
-
+        predictedState[j] = curState[j] + dt * curState[j+3];
+        cout << predictedState[j] << endl;
     }
 
 }   
